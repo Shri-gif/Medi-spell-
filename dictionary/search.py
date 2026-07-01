912150ce7f4d0caa44d8743aab74d9bdaa8a2ec5
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-07-01 23:25:36 +0530

    Update search.py

--- dictionary/search.py
+++ dictionary/search.py
@@ -1,1 +1,31 @@
+import json
+import os
 
+
+class MedicalDictionary:
+
+    def __init__(self):
+
+        current_folder = os.path.dirname(__file__)
+
+        json_path = os.path.join(current_folder, "medical_terms.json")
+
+        with open(json_path, "r", encoding="utf-8") as file:
+            self.words = json.load(file)
+
+    def search(self, text):
+
+        if not text:
+            return []
+
+        text = text.lower()
+
+        result = []
+
+        for word in self.words:
+
+            if word.lower().startswith(text):
+
+                result.append(word)
+
+        return result[:10]
