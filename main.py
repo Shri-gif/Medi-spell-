7cd8ae2ec0162c0212261d626b0e2e53e3de535a
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-07-01 23:02:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import sys
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
 
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("MediSpellAI")
+        self.resize(900, 600)
+
+        layout = QVBoxLayout()
+
+        title = QLabel("Welcome to MediSpellAI")
+        title.setStyleSheet("""
+            font-size:28px;
+            font-weight:bold;
+        """)
+
+        layout.addWidget(title)
+
+        self.setLayout(layout)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+sys.exit(app.exec())
