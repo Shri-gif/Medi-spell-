ca0f1e35043b8205c679b656945872e653337818
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-07-01 23:22:20 +0530

    Update popup.py

--- ui/popup.py
+++ ui/popup.py
@@ -1,1 +1,42 @@
+from PySide6.QtWidgets import QListWidget
+from PySide6.QtCore import Qt
 
+
+class SuggestionPopup(QListWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowFlags(Qt.Popup)
+
+        self.setMinimumWidth(350)
+        self.setMaximumHeight(200)
+
+        self.setStyleSheet("""
+            QListWidget{
+                background:#2C2C2C;
+                color:white;
+                border:1px solid #555;
+                border-radius:8px;
+                font-size:14px;
+                padding:5px;
+            }
+
+            QListWidget::item{
+                padding:8px;
+            }
+
+            QListWidget::item:selected{
+                background:#0A84FF;
+                color:white;
+            }
+        """)
+
+    def show_suggestions(self, words):
+        self.clear()
+
+        if not words:
+            self.hide()
+            return
+
+        self.addItems(words)
+        self.show()
